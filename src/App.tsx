3dde2649f7975477f6f26c9ac36c8ffc8aae989d
raghavendra-019 <23bk5a7206@stpetershyd.com>
2024-12-21 12:25:01 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/home'
+import Movies from './components/movies'
+
+export default function App() {
+  return (
+    <div>
+
+      <Movies />
+    </div>
+  )
+}
